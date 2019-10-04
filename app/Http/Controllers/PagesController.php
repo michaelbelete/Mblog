50ab2a7df2b47783d3569e301163189153effdf7
michaelbelete <it.michael.belete@gmail.com>
2019-10-03 18:14:49 -0800

    passing data from the controller to the view using with
    more for multiple value

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -8,7 +8,8 @@ class PagesController extends Controller
 {
     public function index(){
         $title = 'Welcome to mblog';
-        return view('pages.index', compact('title'));
+        // return view('pages.index', compact('title'));
+        return view('pages.index')->with('title',$title);
     }
 
     public function about(){
