8d1c9df1bf8591669944f2aa32acd02b0ae3b55f
michaelbelete <it.michael.belete@gmail.com>
2019-10-08 14:52:30 -0800

    integrated tinymce wsgi editor to the blog

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,6 +1,9 @@
 @extends('layout.app')
 
 @section('content')
+
+<script src="{{ asset('js/tinymce/tinymce.min.js')}}"></script>
+<script src="{{ asset('js/tinymce/jquery.tinymce.min.js')}}"></script>
     <div class="row container">
         <div class="col-md-12">
             <h1>Create Post</h1>
@@ -12,7 +15,7 @@
                 </div>
                 <div class="form-group">
                     <label for="title">Body</label>
-                    <textarea name="body" class="form-control" cols="30" rows="10" placeholder="enter you body here..."></textarea>
+                    <textarea name="body" id="body" class="form-control" cols="30" rows="10" placeholder="enter you body here..."></textarea>
                 </div>
                 <div class="form-group">
                     <button class="btn btn-primary">Post</button>
@@ -20,4 +23,16 @@
             </form>
         </div>
     </div>
+<script>
+tinymce.init({
+        selector: "#body",
+        plugins: [
+             "advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker",
+             "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
+             "save table contextmenu directionality emoticons template paste textcolor"
+       ],
+       toolbar: "insertfile | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | image  preview media fullpage  forecolor backcolor emoticons",
+
+    });
+</script>
 @endsection
