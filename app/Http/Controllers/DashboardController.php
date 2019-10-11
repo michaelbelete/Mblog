f3d3c49d251b844b6d6ddc12b97f42a6e982e243
michaelbelete <it.michael.belete@gmail.com>
2019-10-11 03:09:48 -0800

    added manage post to dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Post;
 class DashboardController extends Controller
 {
     /**
@@ -23,6 +23,7 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('dashboard');
+        $posts = Post::all();
+        return view('dashboard')->with('posts', $posts);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,21 +17,44 @@
         </div>
     </div>
     <div class="col-md-6">
-        <h1 class="pb-4 text-center">Your Posts</h1>
+        <h3 class="pb-4 pl-2">Manage Your Posts</h3>
         <div class="row pl-4">
-            <div class="col-md-6">
-                <div class="card mb-4 box-shadow">
-                    <img data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22348%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20348%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16da0ec64b3%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A17pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16da0ec64b3%22%3E%3Crect%20width%3D%22348%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22116.71875%22%20y%3D%22120.3%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true" class="card-img-top" style="height: 125px; width: 100%; display: block;">
-                    <div class="card-body">
-                        <h3 class="card-heading">hello world</h3>
-                        <p class="card-text">mike belete</p>
-                        <div class="d-flex justify-content-between align-items-center">
-                            <div class="btn-group"><a href="posts/8" class="btn btn-sm btn-outline-secondary">View</a>
-                            <a href="posts/8/edit" class="btn btn-sm btn-outline-secondary">Edit</a></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <table class="table table-striped table-bordered dataTable no-footer">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>View</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                @if(count($posts) > 0)
+                    @foreach($posts as $post)
+                        <tr>
+                            <td>{{ $post->title}}</td>
+                            <td>
+                                <a href="posts/{{ $post->id }}" class="btn btn-outline-secondary btn-sm">View</a>
+                            </td>
+                            <td>
+                                <a href="posts/{{ $post->id }}/edit" class="btn btn-outline-primary btn-sm">Edit</a>
+                            </td>
+                            <td>
+                                <form action="/posts/{{ $post->id}}" method="post">
+                                    @csrf
+                                    @method("delete")
+                                    <button type="submit" class="btn btn-outline-danger mr-3 btn-sm">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                @else
+                    <tr>
+                        <td colspan="3" class="text-center">There is no post</td>
+                    </tr>
+                @endif
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
